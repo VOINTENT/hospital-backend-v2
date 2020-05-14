eaf2fe6464af6dbb1dc96f59df4cde06c14e9b1e
Andrey Grishin <andrey@rcsmail.ru>
2020-05-14 18:00:45 +0300

    Fixed CORS

--- services/core/src/internal/servers/http/middlewares/middlewares.py
+++ services/core/src/internal/servers/http/middlewares/middlewares.py
@@ -6,5 +6,5 @@ def add_cors(app):
     async def cors(request, response):
         if isawaitable(response):
             response = await response
-        response.headers['Access-Control-Allow-Origin'] = 'localhost:3000'
+        response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
         response.headers['Access-Control-Allow-Credentials'] = True

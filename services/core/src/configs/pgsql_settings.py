56506a982e7b49ed5488c3387395206fa883f5fa
Andrey Grishin <andrey@rcsmail.ru>
2020-05-14 17:39:30 +0300

    Added db venv variables

--- services/core/src/configs/pgsql_settings.py
+++ services/core/src/configs/pgsql_settings.py
@@ -1,5 +1,7 @@
-HOST = '185.27.194.131'
-PORT = 5432
-USER = 'adisen'
-DB_NAME = 'rkib'
-PASSWORD = 'adisen'
+import os
+
+HOST =      os.environ.get('HOSPITAL_DB_HOST')
+PORT =      int(os.environ.get('HOSPITAL_DB_PORT'))
+USER =      os.environ.get('HOSPITAL_DB_USER')
+DB_NAME =   os.environ.get('HOSPITAL_DB_NAME')
+PASSWORD =  os.environ.get('HOSPITAL_DB_PASSWORD')

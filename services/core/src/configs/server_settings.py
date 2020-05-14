59c182cb0c1c58142f7075564fa1c89422fec23b
Andrey Grishin <andrey@rcsmail.ru>
2020-05-14 17:24:13 +0300

    Added env variables

--- services/core/src/configs/server_settings.py
+++ services/core/src/configs/server_settings.py
@@ -1,7 +1,9 @@
-HOST = '127.0.0.1'
-PORT = 8000
-DEBUG = True
-SSL = None
-WORKERS = 1
-ACCESS_LOG = True
-AUTO_RELOAD = False
+import os
+
+HOST        = os.environ.get('HOSPITAL_SERVER_HOST')
+PORT        = int(os.environ.get('HOSPITAL_SERVER_PORT'))
+DEBUG       = bool(int(os.environ.get('HOSPITAL_SERVER_DEBUG')))
+SSL         = None
+WORKERS     = int(os.environ.get('HOSPITAL_SERVER_WORKERS'))
+ACCESS_LOG  = bool(int(os.environ.get('HOSPITAL_SERVER_ACCESS_LOG')))
+AUTO_RELOAD = bool(int(os.environ.get('HOSPITAL_SERVER_AUTO_RELOAD')))

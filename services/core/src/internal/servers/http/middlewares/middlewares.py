c16e0440d2de8182aca9b391e942ae5eae2ce2c4
Andrey Grishin <andrey@rcsmail.ru>
2020-05-14 18:54:42 +0300

    Fixed CORS

--- services/core/src/internal/servers/http/middlewares/middlewares.py
+++ services/core/src/internal/servers/http/middlewares/middlewares.py
@@ -9,7 +9,7 @@ def add_cors(app):
             response = await response
         response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000' 
         response.headers['Access-Control-Allow-Credentials'] = 'true'
-        response.headers['Access-Control-Request-Methods'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, DELETE'
 
     @app.middleware('request')
     async def cors_options(request):
